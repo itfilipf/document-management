b197751b96842b3e6c2c36ca51f3c213a56f40c9
itfilipf <itfiipf@gmail.com>
2025-09-18 09:34:49 +0200

    Register document share endpoint

--- src/propylon_document_manager/site/api_router.py
+++ src/propylon_document_manager/site/api_router.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from propylon_document_manager.file_versions.api.views import FileVersionViewSet, DocumentView, DocumentListView, \
-    DocumentByHashView
+    DocumentByHashView, DocumentShareView
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -16,5 +16,7 @@ app_name = "api"
 urlpatterns = router.urls + [
     path("documents/", DocumentListView.as_view(), name="document-list"),
     path("documents/hash/<str:content_hash>/", DocumentByHashView.as_view(), name="document-by-hash"),
+    path("documents/hash/<str:content_hash>/share/", DocumentShareView.as_view(), name="document-share"),
     path("documents/<path:url>/", DocumentView.as_view(), name="document"),
+
 ]
